09a24ee525f2121901f468adf36bb37bc4302eb2
Kevin <fraesh93@gmail.com>
2023-08-28 19:02:31 +0200

    Updates

---
+++ _app/immutable/nodes/6.6b2a3a61.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.6b4a5b57.js";import{S as t,i as e}from"../chunks/index.093f3364.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
