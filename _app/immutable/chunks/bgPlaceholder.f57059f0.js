5e7861e7f07c6263d2aa912777be18133de6960b
Kevin <fraesh93@gmail.com>
2024-03-13 17:14:10 +0100

    Updates

--- _app/immutable/chunks/bgPlaceholder.f57059f0.js
+++
@@ -1,1 +0,0 @@
-const e=""+new URL("../assets/bgPlaceholder.9e5a8fcb.jpg",import.meta.url).href;export{e as P};
---
+++ _app/immutable/chunks/navigation.0b01c535.js
@@ -0,0 +1,1 @@
+import{h as o}from"./singletons.16f0ecdc.js";const e=o("goto");export{e as g};
